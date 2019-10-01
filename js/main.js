e1ceaa428ceec0e4d7818daef73f22e95105b91e
Kasia Kordys <kasia.kordys@gmail.com>
2019-10-01 13:58:15 +0100

    Add project files with content and style

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+var hotelInfo;
+var details;
+var xhr = new XMLHttpRequest();
+xhr.open('GET', "data.json", true);
+xhr.responseType = 'text';
+xhr.send();
+
+
+xhr.onload = function() {
+    if(xhr.status === 200) {
+        hotelInfo = JSON.parse(xhr.responseText);
+        console.log(hotelInfo);
+  
+    } // end if
+} // end function

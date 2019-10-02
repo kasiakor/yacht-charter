1a356bd270e11c243437121b8a3a4ece98fca49f
Kasia Kordys <kasia.kordys@gmail.com>
2019-10-02 18:36:45 +0100

    Add function to display JSON data

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-var hotelInfo;
+var yachtInfo;
 var details;
 var xhr = new XMLHttpRequest();
 xhr.open('GET', "data.json", true);
@@ -7,10 +7,30 @@ xhr.responseType = 'text';
 xhr.send();
 
 
-xhr.onload = function() {
+xhr.onload = function(){
     if(xhr.status === 200) {
-        hotelInfo = JSON.parse(xhr.responseText);
-        console.log(hotelInfo);
+        yachtInfo = JSON.parse(xhr.responseText);
+        console.log(yachtInfo);
+        display(0);
   
     } // end if
 } // end function
+
+// function to display JSON data
+function display(x){
+    console.log(x);
+    document.getElementById("yachtName").innerHTML = yachtInfo[x].name;
+    document.getElementById("desc").innerHTML = yachtInfo[x].description;
+    document.getElementById("photo").src = yachtInfo[x].photo;
+    document.getElementById("highs").innerHTML = yachtInfo[x].cost.HS;
+    document.getElementById("lows").innerHTML = yachtInfo[x].cost.LS;
+
+    details = "";
+
+    for (i=0; i < yachtInfo[x].details.length; i++ ){
+        console.log(yachtInfo[x].details[i]);
+        details += "<p>" + yachtInfo[x].details[i] + "</p>";
+    } // end loop
+    document.getElementById("details").innerHTML = details;
+} // end function
+
